4673f5c61baac43ce679890534d6169fe3d23465
Gabriele Destefanis <g.destefanis@topsolution.it>
2017-10-27 10:10:01 +0200

    Removed vendor imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import angular from 'angular';
-
 import {hello} from './app/hello/hello';
 import {footer} from './app/footer/footer';
-import '@uirouter/angularjs';
 import routesConfig from './routes';
 
 import './index.scss';
